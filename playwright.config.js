fad2c6da9f667295aea6650254e142d76f9f7152
test-amit <>
2026-03-02 11:38:07 +0530

    allure report integration

--- playwright.config.js
+++ playwright.config.js
@@ -4,8 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
+
+  timeout: 60000,
+
   use: {
     baseURL: process.env.BASE_URL,
-    headless: false
-  }
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
+  },
+
+  reporter: [
+    ['list'],
+    ['allure-playwright']
+  ]
+
 });
